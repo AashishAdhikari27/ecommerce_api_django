f7a03ed06a165b88b163441b2bde17272b0b64c5
AashishAdhikari27 <aashishadhikari2027@gmail.com>
2025-08-17 23:00:42 +0545

    simplejwt initiated

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -168,6 +168,11 @@ REST_FRAMEWORK = {
 
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+       "rest_framework_simplejwt.authentication.JWTAuthentication",
+       )
+
+       
 }
 
 
